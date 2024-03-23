02263a8c5bc038b6f18b5fc29edf98e773a956a9
Juhye Son <juhye.son990902@gmail.com>
2024-03-23 18:16:08 +0900

    save: 2024년 3월 23일 토요일 18시 16분 08초 KST

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+cd public
+git checkout main
+git pull
+rm -rf `ls -a | grep -v .git`
+cd ..
+echo "[[[ BUILD ]]]"
+hugo -D --minify
+cd public
+git add .
+git commit -m "deploy: $(date)"
+git push
+cd ..
+bash push.sh
---
+++ pull.sh
@@ -0,0 +1,3 @@
+git pull
+cd public
+git pull origin main
